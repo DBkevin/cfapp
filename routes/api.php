7ac7a1c7a28a7a46fd712b734d73d1ffcf1f1092
Lee <416606903@outlook.com>
2020-09-16 04:00:10 +0000

    安装初始auth

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,17 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+*/
+
+/**
+ * v1前缀命名组，api默认有，所以真实路径为localhost.com/api/v1
+ */
+Route::prefix('v1')->group(function () {
+    Route::get('login', function () {
+        return 'aaa';
+    });
 });
